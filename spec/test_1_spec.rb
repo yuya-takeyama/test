f4dd8e709eb9d7b38908bfdb381b6752bce4e779
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2017-06-08 19:46:37 +0900

    Multiple JUnit XML files

---
+++ spec/test_1_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe 'Test1' do
+  it 'fails 1' do
+    expect(true).to be false
+  end
+end
---
+++ spec/test_2_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe 'Test 2' do
+  it 'fails 2' do
+    expect(false).to be true
+  end
+end
